9c2a830bafee01642b52ccc5c0b05c5a91cfd0cf
andomeder <obinowilliam@gmail.com>
2025-04-20 02:19:45 +0300

    Added constants

---
+++ lib/configs/constants.dart
@@ -0,0 +1,8 @@
+const String pipedInsstanceUrl = 'https://piped.kavin.rocks';
+
+// Secure storage keys
+const String secureStorageAuthTokenKey = 'authToken';
+const String secureStorageUserIdKey = 'userId';
+
+// GetStorage keys
+const String storageUsernameKey = 'lastUsername';
