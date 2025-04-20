2fa5531144f683f416bf792bb27fb968cd113d51
andomeder <obinowilliam@gmail.com>
2025-04-20 15:35:29 +0300

    Feat: LoadingIndicator added

---
+++ lib/views/widgets/common/loading_indicator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatefulWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  State<LoadingIndicator> createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator(
+      color: Theme.of(context).colorScheme.primary,
+    );
+  }
+}
