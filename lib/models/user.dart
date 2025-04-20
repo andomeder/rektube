915326020cdeec0fe78b5f318fa3ebbc5e01f99a
andomeder <obinowilliam@gmail.com>
2025-04-20 13:25:44 +0300

    Added user model

---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import 'package:rektube/database/database.dart';
+
+class AppUser extends Equatable {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String username;
+  final String email;
+
+  const AppUser({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.username,
+    required this.email,
+  });
+
+  // Optional: Factory constructor from Drift's User data class
+  factory AppUser.fromDbUser(User dbUser) {
+    return AppUser(
+      id: dbUser.id,
+      firstName: dbUser.firstName,
+      lastName: dbUser.lastName,
+      username: dbUser.username,
+      email: dbUser.email,
+    );
+  }
+  // Equatable implementation
+  @override
+  List<Object?> get props => [id, firstName, lastName, username, email];
+}
